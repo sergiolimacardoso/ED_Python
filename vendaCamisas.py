909efa4070aaada910bc766645cbd65d7211ed31
Sergio <sergiolimacardoso@hotmail.com>
2025-07-21 17:15:33 -0300

    Add files via upload

---
+++ vendaCamisas.py
@@ -0,0 +1,6 @@
+camisa = float(input("Digite o preço da camisa: "))
+valor_camisa = int(input("Digite a quantidade camisas: "))
+
+valor_compra = camisa * valor_camisa
+
+print("O valor da compra é: ", float(valor_compra))
